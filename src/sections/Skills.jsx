ad0e44440562a4f56e2d3dbab2dee1610e51feb6
Steadydrumz <steadydrums14@gmail.com>
2026-03-17 13:46:47 +0100

    Refine Skills component: removed outdated items and added new skills

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -44,7 +44,7 @@ const Skills = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="3"></circle><line x1="3" x2="9" y1="12" y2="12"></line><line x1="15" x2="21" y1="12" y2="12"></line></svg>
       ),
       color: "bg-red-500",
-      skills: ["Git", "GitHub", "GitLab", "Vercel", "Netlify"]
+      skills: ["Git", "GitHub", "GitLab", "Bitbucket" ]
     }
   ];
 
